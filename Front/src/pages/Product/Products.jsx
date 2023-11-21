b206d8aecadbc40639bd1bcc34577b7bee9c0979
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2023-11-21 21:43:32 +0900

    정렬 아직 애매함

--- Front/src/pages/Product/Products.jsx
+++ Front/src/pages/Product/Products.jsx
@@ -13,12 +13,18 @@ import ProductCard from "../../components/ui/ProductCard";
 
 const Products = () => {
     const [filters, setFilters] = useState([])
+    const [isMenuOpen, setMenuOpen] = useState(false);
     const {data, isLoading, error} = useFetchProducts()
     console.log("ddddsdsa+=",data?.title)
     console.log("dsdadwdqeq__", data?.name)
 
     const [isOpen, setIsOpen] = useState(true); // 상태 추가: 패널이 열려있는지 여부
     const [isOpen2, setIsOpen2] = useState(false);
+
+
+    const handleToggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
 
     const handleToggle2 = () => {
         setIsOpen2(!isOpen2);
@@ -49,24 +55,43 @@ const Products = () => {
                     <div className="flex items-center">
                         <div className="relative inline-block text-left">
                             <div>
-                                <button type="button" className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900" id="menu-button" aria-expanded="false" aria-haspopup="true">
+                                <button
+                                    type="button"
+                                    className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900"
+                                    id="menu-button"
+                                    aria-expanded={isMenuOpen}
+                                    aria-haspopup="true"
+                                    onClick={handleToggleMenu}
+                                    >
                                     Sort
-                                    <svg className="-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                        <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                                    <svg
+                                        className={`-mr-1 ml-1 h-5 w-5 flex-shrink-0 text-gray-400 group-hover:text-gray-500 ${
+                                            isMenuOpen ? 'rotate-180' : ''
+                                        }`}
+                                        viewBox="0 0 20 20" fill="currentColor"
+                                        aria-hidden="true"
+                                    >
+                                        <path
+                                            fill-rule="evenodd"
+                                            d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                            clip-rule="evenodd"
+                                        />
                                     </svg>
                                 </button>
                             </div>
                         </div>
                     </div>
-                    <div class="absolute right-0 z-10 mt-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-                        <div class="py-1" role="none">
-                            <a href="#" class="font-medium text-gray-900 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Most Popular</a>
-                            <a href="#" class="text-gray-500 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Best Rating</a>
-                            <a href="#" class="text-gray-500 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Newest</a>
-                            <a href="#" class="text-gray-500 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-3">Price: Low to High</a>
-                            <a href="#" class="text-gray-500 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-4">Price: High to Low</a>
+                    {isMenuOpen && (
+                        <div className="absolute right-0 z-10 mb-2 w-40 origin-top-right rounded-md bg-white shadow-2xl ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1">
+                            <div className="py-1" role="none">
+                                <a href="#" className="font-medium text-gray-900 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">Most Popular</a>
+                                <a href="#" className="text-gray-500 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-1">Best Rating</a>
+                                <a href="#" className="text-gray-500 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-2">Newest</a>
+                                <a href="#" className="text-gray-500 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-3">Price: Low to High</a>
+                                <a href="#" class="text-gray-500 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-4">Price: High to Low</a>
+                            </div>
                         </div>
-                    </div>
+                    )}
                     {/*<button*/}
                     {/*    type="button"*/}
                     {/*    className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"*/}
