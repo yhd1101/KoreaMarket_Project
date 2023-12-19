3afc1e70e0d0a1fd3a80ca5f7d5abd9149f4c693
양한동융합소프트웨어전공 <yhd0121@gs.anyang.ac.kr>
2023-12-19 16:51:49 +0900

    redis delete

--- Backend/src/product/product.service.ts
+++ Backend/src/product/product.service.ts
@@ -27,6 +27,8 @@ export class ProductService {
     });
 
     await this.productRepository.save(newProduct);
+    //redis data 삭제
+    await this.cacheManager.del("products")
     return newProduct;
   }
 
@@ -50,9 +52,13 @@ export class ProductService {
     pageOptionsDto: PageOptionsDto,
     category?: string | string[],
   ): Promise<PageDto<Product>> {
+    //1.전체 프로덕트 api를 Call 했을때 redis에 정보를 조회
     const redisData = await this.cacheManager.get("products")
-    console.log( redisData)
+
+
+    //2.redis정보가 있으면 redis에 데이터를 리턴함
     if(redisData) {
+      console.log( redisData.length)
       return redisData
     }
     const queryBuilder = await this.productRepository.createQueryBuilder(
@@ -106,6 +112,7 @@ export class ProductService {
     const { entities } = await queryBuilder.getRawAndEntities();
 
     // await this.cacheManager.get(entities)
+    //3.redis 정보가 없으면 db에서 콜하고 redis에 저장후 return (set 저장해줌)
     await this.cacheManager.set("products", entities)
 
 
@@ -119,34 +126,89 @@ export class ProductService {
   }
 
   async productGetById(id: string) {
+    const redisData = await this.cacheManager.get("products")
+
     const product = await this.productRepository
-      .createQueryBuilder('product')
-      .leftJoinAndSelect('product.seller', 'seller')
-      .leftJoinAndSelect('product.comments', 'comments')
-      .leftJoinAndSelect('comments.user', 'user')
-      .where('product.id = :id', { id })
-      .getOne();
-    // const product = await this.productRepository.findOneBy({
-    //   where: { id },
-    //   relations: {
-    //     seller: true,
-    //   },
-    // });
-    if (!product) {
-      // id가 없을때 만들어줌
-      throw new HttpException('No id', HttpStatus.NOT_FOUND);
+        .createQueryBuilder('product')
+        .leftJoinAndSelect('product.seller', 'seller')
+        .leftJoinAndSelect('product.comments', 'comments')
+        .leftJoinAndSelect('comments.user', 'user')
+        .where('product.id = :id', {id})
+        .getOne();
+
+
+
+    if (redisData !== null) {
+      console.log("+++++")
+      const filteredData = await redisData.filter((item: any) => item.id === id)
+      console.log("*****",filteredData)
+      return filteredData
+
+
     }
+    // if (!product) {
+    //   // id가 없을때 만들어줌
+    //   throw new HttpException('No id', HttpStatus.NOT_FOUND);
+    // }
+    console.log("-------")
     return product;
+
+
+
+
+
+
+      // if (Array.isArray(redisData)) {
+      //   const ids = redisData.map(item => item.id);
+      //   console.log("IDs:", ids);
+      // } else {
+      //   console.log("올바르지 않은 데이터 형식입니다.");
+      // }
+
+      // const ids = redisData.map(item => item.id);
+      // const reData = redisData.map(item => item)
+      // console.log("data",reData)
+      // // console.log("IDs:", ids);
+      //
+      // for (let i = 0; i< ids.length; i++) {
+      //   if (ids === id) {
+      //
+      //   }
+      // }
+
+
+
+      // const product = await this.productRepository.findOneBy({
+      //   where: { id },
+      //   relations: {
+      //     seller: true,
+      //   },
+      // });
+
+
+
+
   }
 
   async productDeleteById(id: string) {
+    // 캐시에서 데이터를 가져옴
+    const redisData = await this.cacheManager.get("products");
+
+    if (redisData !== null) {
+      // Redis에서 데이터를 삭제
+      await this.cacheManager.del('products');
+      await this.cacheManager.get("products")
+
+    }
+
+    // 데이터베이스에서 데이터를 삭제
     await this.productRepository.delete({ id });
-    return 'deleted product';
+
+    return "deleted";
   }
+
   async productUpdateById(id: string, createProductDto: CreateProductDto) {
-    // const product = await this.productRepository.findOneBy({ id });
-    // const updateProduct = await this.productRepository.update(createProductDto);
-    // return updateProduct;
+
     await this.productRepository.update(id, createProductDto);
     return 'updated product';
   }
